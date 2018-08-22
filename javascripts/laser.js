c2b100d7b7cc36c90848814b2ce105ff1bc53797
Aidan Gadberry <agadberr@gmail.com>
2018-08-21 18:05:57 -0700

    Refactor code into parent class

--- javascripts/laser.js
+++ javascripts/laser.js
@@ -1,17 +1,17 @@
-class Laser {
+import Sprite from './sprite';
+
+class Laser extends Sprite {
   constructor(pos, ctx, dir = 'E') {
+    super(pos, ctx, dir);
     this.url = "images/laser_entity.png";
-    this.size = [50, 20];
-    this.pos = pos;
-    this.dir = dir;
   }
 
   render(ctx) {
     const img = new Image();
     img.src = this.url;
-    
-    img.onload = function() {
-      ctx.drawImage(img, 50, 50, 50, 20);
+
+    img.onload = () => {
+      ctx.drawImage(img, this.pos[0], this.pos[1]);
     }
   }
 }
---
+++ javascripts/sprite.js
@@ -0,0 +1,9 @@
+class Sprite {
+  constructor(pos, ctx, dir) {
+    this.size = [50, 20];
+    this.pos = pos;
+    this.dir = dir;
+  }
+}
+
+export default Sprite;
