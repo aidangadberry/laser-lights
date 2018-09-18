9b0b8f3bb7486b15307eeeb75729109e593fd2f2
Aidan Gadberry <agadberr@gmail.com>
2018-09-18 10:52:33 -0700

    Add background

--- bundle.js
+++ bundle.js
@@ -134,7 +134,7 @@ function drawBeam(startPos, endPos) {
   ctx.shadowBlur = 15;
   ctx.shadowColor = "#F00"
   
-  ctx.lineWidth = 1;
+  ctx.lineWidth = 1.5;
   ctx.stroke();
 }
 
@@ -171,7 +171,7 @@ const getBeams = (laser, entities) => {
   let beamEnd = false;
   let endPos;
   let startPos = laserPos.slice(0);
-  
+
   while (beamEnd === false) {
     ({endPos, angle, beamEnd} = getBeamCollision(startPos, angle, entities));
     drawBeam(startPos, endPos);
@@ -202,22 +202,38 @@ __webpack_require__.r(__webpack_exports__);
 
 class Game {
   constructor(canvas) {
-    this.entities = [];
     this.canvas = canvas;
     this.ctx = canvas.getContext('2d');
+    
+    this.entities = [];
+    this.currentEntity;
 
     this.mouseX;
     this.mouseY;
     this.dragging = false;
     this.dragX;
     this.dragY;
-
-    this.currentEntity;
-
-
-    this.addListeners();
   }
 
+  run() {
+    this.addListeners();
+
+    this.addLaser(200, 200, 312);
+    this.addLaser(430, 300, 140);
+    this.addMirror(300, 100);
+    this.addMirror(310, 130, 180);
+    this.addMirror(100, 400, 270);
+    this.addMirror(400, 40);
+    this.addMirror(210, 500, 180);
+    this.resizeCanvas();
+    this.currentEntity = this.entities[0];
+  }
+  
+  resizeCanvas() {
+    this.scaleByDevicePixelRatio(window.innerWidth, window.innerHeight)
+    this.drawGame();
+  }
+  
   scaleByDevicePixelRatio(canvasSizeX, canvasSizeY) {
     this.canvas.style.width = canvasSizeX + "px";
     this.canvas.style.height = canvasSizeY + "px";
@@ -229,39 +245,52 @@ class Game {
     this.ctx.scale(scale, scale);
   }
 
-  resizeCanvas() {
-    console.log("resize");
-    this.scaleByDevicePixelRatio(window.innerWidth, window.innerHeight)
+  drawGame() {
+    this.ctx.fillStyle = "#EEE";
+    this.ctx.rect(0, 0, this.canvas.width, this.canvas.height);
+    this.ctx.fill();
 
+    this.drawBackground();
+    this.renderBeams();
     this.renderEntities();
   }
 
-  run() {
-    this.addLaser(200, 200, 312);
-    this.addLaser(430, 300, 140);
-    this.addMirror(300, 100);
-    this.addMirror(310, 130, 180);
-    this.addMirror(100, 400, 270);
-    this.addMirror(400, 40);
-    this.addMirror(210, 500, 180);
-    this.resizeCanvas();
-    this.currentEntity = this.entities[0];
+  renderEntities() {
+    for (var i = 0; i < this.entities.length; i++) {
+      this.entities[i].draw();
+    }
   }
 
-  renderEntities() {
-    this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
-
+  renderBeams() {
     for (var i = 0; i < this.entities.length; i++) {
       const entity = this.entities[i];
 
       if (entity instanceof _laser__WEBPACK_IMPORTED_MODULE_0__["default"]) {
-        // if (!this.dragging) {
-          Object(_beam__WEBPACK_IMPORTED_MODULE_2__["getBeams"])(entity, this.entities);
-        // }
+        Object(_beam__WEBPACK_IMPORTED_MODULE_2__["getBeams"])(entity, this.entities);
       }
-
-      entity.draw();
     }
+  }
+
+  drawBackground() {
+    this.ctx.strokeStyle = "#AAA";
+    this.ctx.lineWidth = 2;
+    this.ctx.globalAlpha = 0.2;
+
+    for (var i = 0; i < this.canvas.width; i += 30) {
+      this.ctx.beginPath();
+      this.ctx.moveTo(i, 0);
+      this.ctx.lineTo(i, this.canvas.height);
+      this.ctx.stroke();
+    }
+
+    for (var i = 0; i < this.canvas.height; i += 30) {
+      this.ctx.beginPath();
+      this.ctx.moveTo(0, i);
+      this.ctx.lineTo(this.canvas.width, i);
+      this.ctx.stroke();
+    }
+
+    this.ctx.globalAlpha = 1;
   }
 
   addLaser(x, y, deg = 0) {
@@ -284,30 +313,30 @@ class Game {
     this.dragEntity = this.dragEntity.bind(this);
     this.endDrag = this.endDrag.bind(this);
     this.resizeCanvas = this.resizeCanvas.bind(this);
-
-    document.addEventListener("keypress", event => {
-      switch (event.code) {
-        case "ArrowLeft":
-        case "KeyA":
-          this.currentEntity.rotateSprite("counterclockwise")
-          this.renderEntities();
-          break;
-        case "ArrowRight":
-        case "KeyD":
-          this.currentEntity.rotateSprite("clockwise");
-          this.renderEntities();
-          break;
-        default:
-      }
-    });
+    this.controls = this.controls.bind(this);
 
     window.addEventListener("resize", this.resizeCanvas);
+    document.addEventListener("keydown", this.controls);
     document.addEventListener("mousedown", this.startDrag);
   }
 
+  controls(e) {
+    switch (e.code) {
+      case "ArrowLeft":
+      case "KeyA":
+        this.currentEntity.rotateSprite("counterclockwise")
+        this.drawGame();
+        break;
+      case "ArrowRight":
+      case "KeyD":
+        this.currentEntity.rotateSprite("clockwise");
+        this.drawGame();
+        break;
+      default:
+    }
+  }
+
   startDrag(e) {
-    console.log("Start Drag");
-    
     e.stopPropagation();
     this.setCursorPosition(e);
 
@@ -316,7 +345,8 @@ class Game {
       
       if (Object(_util__WEBPACK_IMPORTED_MODULE_3__["collidesWithObject"])([this.mouseX, this.mouseY], entity)) {
         this.currentEntity = entity;
-        // ADD ACTIVE CLASS TO CURRENT SPRITE ICON
+        this.entities.splice(i, 1)
+        this.entities.push(entity);
 
         this.dragging = true;
         this.dragX = this.mouseX - entity.x;
@@ -333,34 +363,26 @@ class Game {
   }
 
   dragEntity(e) {
-    console.log("Move Mouse");
     this.setCursorPosition(e);
     
     this.currentEntity.x = this.mouseX - this.dragX;
     this.currentEntity.y = this.mouseY - this.dragY;
 
-    this.renderEntities();
+    this.drawGame();
   }
 
   endDrag(e) {
-    console.log("End Drag");
     
     document.addEventListener("mousedown", this.startDrag, false);
     document.removeEventListener("mouseup", this.endDrag, false);
 
     if (this.dragging) {
       this.dragging = false;
-      console.log("Remove");
       document.removeEventListener("mousemove", this.dragEntity, false);
     }
 
-    this.renderEntities();
+    this.drawGame();
   }
-
-
-  // onMouseMove(e) {
-  //   e.stopPropagation();
-  // }
 }
 
 /* harmony default export */ __webpack_exports__["default"] = (Game);
@@ -541,10 +563,11 @@ const isInBounds = pos => {
 const pointIsOnMirrorEdge = (pos, mirror) => {
   const x0 = getRotatedPos(pos, mirror.getCenterPos(), -mirror.rad)[0];
   const y0 = getRotatedPos(pos, mirror.getCenterPos(), -mirror.rad)[1];
-  const x1 = mirror.corners()[0][0];
-  const y1 = mirror.corners()[0][1];
-  const x2 = mirror.corners()[1][0];
-  const y2 = mirror.corners()[1][1];
+  const corners = mirror.corners();
+  const x1 = corners[0][0];
+  const y1 = corners[0][1];
+  const x2 = corners[1][0];
+  const y2 = corners[1][1];
 
   const dist = Math.abs((y2 - y1)*x0 - (x2 - x1)*y0 + x2*y1 - y2*x1) / mirror.width;
 
--- javascripts/game.js
+++ javascripts/game.js
@@ -5,22 +5,38 @@ import {pointIsOnMirrorEdge, collidesWithObject} from './util';
 
 class Game {
   constructor(canvas) {
-    this.entities = [];
     this.canvas = canvas;
     this.ctx = canvas.getContext('2d');
+    
+    this.entities = [];
+    this.currentEntity;
 
     this.mouseX;
     this.mouseY;
     this.dragging = false;
     this.dragX;
     this.dragY;
-
-    this.currentEntity;
-
-
-    this.addListeners();
   }
 
+  run() {
+    this.addListeners();
+
+    this.addLaser(200, 200, 312);
+    this.addLaser(430, 300, 140);
+    this.addMirror(300, 100);
+    this.addMirror(310, 130, 180);
+    this.addMirror(100, 400, 270);
+    this.addMirror(400, 40);
+    this.addMirror(210, 500, 180);
+    this.resizeCanvas();
+    this.currentEntity = this.entities[0];
+  }
+  
+  resizeCanvas() {
+    this.scaleByDevicePixelRatio(window.innerWidth, window.innerHeight)
+    this.drawGame();
+  }
+  
   scaleByDevicePixelRatio(canvasSizeX, canvasSizeY) {
     this.canvas.style.width = canvasSizeX + "px";
     this.canvas.style.height = canvasSizeY + "px";
@@ -32,39 +48,52 @@ class Game {
     this.ctx.scale(scale, scale);
   }
 
-  resizeCanvas() {
-    console.log("resize");
-    this.scaleByDevicePixelRatio(window.innerWidth, window.innerHeight)
+  drawGame() {
+    this.ctx.fillStyle = "#EEE";
+    this.ctx.rect(0, 0, this.canvas.width, this.canvas.height);
+    this.ctx.fill();
 
+    this.drawBackground();
+    this.renderBeams();
     this.renderEntities();
   }
 
-  run() {
-    this.addLaser(200, 200, 312);
-    this.addLaser(430, 300, 140);
-    this.addMirror(300, 100);
-    this.addMirror(310, 130, 180);
-    this.addMirror(100, 400, 270);
-    this.addMirror(400, 40);
-    this.addMirror(210, 500, 180);
-    this.resizeCanvas();
-    this.currentEntity = this.entities[0];
+  renderEntities() {
+    for (var i = 0; i < this.entities.length; i++) {
+      this.entities[i].draw();
+    }
   }
 
-  renderEntities() {
-    this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
-
+  renderBeams() {
     for (var i = 0; i < this.entities.length; i++) {
       const entity = this.entities[i];
 
       if (entity instanceof Laser) {
-        // if (!this.dragging) {
-          getBeams(entity, this.entities);
-        // }
+        getBeams(entity, this.entities);
       }
-
-      entity.draw();
     }
+  }
+
+  drawBackground() {
+    this.ctx.strokeStyle = "#AAA";
+    this.ctx.lineWidth = 2;
+    this.ctx.globalAlpha = 0.2;
+
+    for (var i = 0; i < this.canvas.width; i += 30) {
+      this.ctx.beginPath();
+      this.ctx.moveTo(i, 0);
+      this.ctx.lineTo(i, this.canvas.height);
+      this.ctx.stroke();
+    }
+
+    for (var i = 0; i < this.canvas.height; i += 30) {
+      this.ctx.beginPath();
+      this.ctx.moveTo(0, i);
+      this.ctx.lineTo(this.canvas.width, i);
+      this.ctx.stroke();
+    }
+
+    this.ctx.globalAlpha = 1;
   }
 
   addLaser(x, y, deg = 0) {
@@ -87,30 +116,30 @@ class Game {
     this.dragEntity = this.dragEntity.bind(this);
     this.endDrag = this.endDrag.bind(this);
     this.resizeCanvas = this.resizeCanvas.bind(this);
-
-    document.addEventListener("keypress", event => {
-      switch (event.code) {
-        case "ArrowLeft":
-        case "KeyA":
-          this.currentEntity.rotateSprite("counterclockwise")
-          this.renderEntities();
-          break;
-        case "ArrowRight":
-        case "KeyD":
-          this.currentEntity.rotateSprite("clockwise");
-          this.renderEntities();
-          break;
-        default:
-      }
-    });
+    this.controls = this.controls.bind(this);
 
     window.addEventListener("resize", this.resizeCanvas);
+    document.addEventListener("keydown", this.controls);
     document.addEventListener("mousedown", this.startDrag);
   }
 
+  controls(e) {
+    switch (e.code) {
+      case "ArrowLeft":
+      case "KeyA":
+        this.currentEntity.rotateSprite("counterclockwise")
+        this.drawGame();
+        break;
+      case "ArrowRight":
+      case "KeyD":
+        this.currentEntity.rotateSprite("clockwise");
+        this.drawGame();
+        break;
+      default:
+    }
+  }
+
   startDrag(e) {
-    console.log("Start Drag");
-    
     e.stopPropagation();
     this.setCursorPosition(e);
 
@@ -119,7 +148,8 @@ class Game {
       
       if (collidesWithObject([this.mouseX, this.mouseY], entity)) {
         this.currentEntity = entity;
-        // ADD ACTIVE CLASS TO CURRENT SPRITE ICON
+        this.entities.splice(i, 1)
+        this.entities.push(entity);
 
         this.dragging = true;
         this.dragX = this.mouseX - entity.x;
@@ -136,34 +166,26 @@ class Game {
   }
 
   dragEntity(e) {
-    console.log("Move Mouse");
     this.setCursorPosition(e);
     
     this.currentEntity.x = this.mouseX - this.dragX;
     this.currentEntity.y = this.mouseY - this.dragY;
 
-    this.renderEntities();
+    this.drawGame();
   }
 
   endDrag(e) {
-    console.log("End Drag");
     
     document.addEventListener("mousedown", this.startDrag, false);
     document.removeEventListener("mouseup", this.endDrag, false);
 
     if (this.dragging) {
       this.dragging = false;
-      console.log("Remove");
       document.removeEventListener("mousemove", this.dragEntity, false);
     }
 
-    this.renderEntities();
+    this.drawGame();
   }
-
-
-  // onMouseMove(e) {
-  //   e.stopPropagation();
-  // }
 }
 
 export default Game;
